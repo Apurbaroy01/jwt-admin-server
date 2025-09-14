372e725c82a2c71fd8ca5b833c6e9c576908d12e
Apurbaroy01 <apurbaroy00011@gmail.com>
2025-09-15 00:50:00 +0600

    2nd

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.User_DB}:${process.env.User_PASS}@cluster0.xgolbpd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,15 +27,43 @@ async function run() {
 
         const userCollation = client.db("jwt_admin").collection("users");
 
-        app.post('/user', async(req, res) => {
+        app.post('/user', async (req, res) => {
             const user = req.body;
-            const result= await userCollation.insertOne(user)
+            const result = await userCollation.insertOne(user)
             res.send(result)
         });
-        app.get('/user', async(req, res) => {
-            const result= await userCollation.find().toArray()
+
+        app.get('/user', async (req, res) => {
+            const result = await userCollation.find().toArray()
             res.send(result)
         });
+
+        app.patch('/user/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await userCollation.updateOne(filter, updateDoc);
+            res.send(result)
+
+        });
+
+
+        app.get('/user/admin/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = {email: email}
+            const user= await userCollation.findOne(query);
+            let admin = false;
+            if(user){
+                admin= user.role === 'admin'
+            }
+            res.send({admin})
+        })
+
 
 
 
